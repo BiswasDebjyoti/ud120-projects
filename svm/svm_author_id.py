b7d0d3dd832d9c006474aacb3d0f8467418503dc
BiswasDebjyoti <debjyoti.biswas.che14@iitbhu.ac.in>
2017-10-19 00:08:41 +0530

    With 1% dataset predicted the output for 50th testcase

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,16 +24,16 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
-#features_train = features_train[:len(features_train)/100] 
-#labels_train = labels_train[:len(labels_train)/100] 
+features_train = features_train[:len(features_train)/100] 
+labels_train = labels_train[:len(labels_train)/100] 
 
 svf = svm.SVC(C=10000,kernel="rbf")
 
 svf.fit(features_train,labels_train)
-pred= svf.predict(features_test)
-accuracy = accuracy_score(labels_test, pred)
+pred= svf.predict(features_test[50])
+#accuracy = accuracy_score(labels_test, pred)
 
-print accuracy
+print pred
 
 #########################################################
 
